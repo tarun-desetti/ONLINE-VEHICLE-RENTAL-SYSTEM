af7e16c78f6f346063a43f78464aa64308500991
tarun-desetti <desettitarun.23.cse@anits.edu.in>
2025-08-14 16:13:59 +0530

    Add files via upload

---
+++ INCLUDES/footer.php
@@ -0,0 +1,19 @@
+	<li class="about">
+					<p>Ours is an organized company that rents cars and other vehicles to clients at lower costs. We we are here to serve every indian Citizen</p>
+					<ul>
+						<li><a href="https://www.facebook.com/groups/100195466697910/" class="facebook" target="_blank"></a></li>
+						<li><a href="#" class="twitter" ></a></li>
+						<li><a href="#" class="google" ></a></li>
+						<li><a href="#" class="skype"></a></li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+
+		<div class="copyrights wrapper">
+			Copyright &copy; <?php echo date("Y")?> All Rights Reserved | Designed by Team 5🦋.
+		</div>
+	</footer><!--  end footer  -->
+	
+</body>
+</html>
